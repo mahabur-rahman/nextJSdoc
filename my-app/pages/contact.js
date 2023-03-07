d14ee0b48c5947e66d3a9eaed4faade339e85176
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-03-07 20:15:51 +0600

    global and module css apply

---
+++ my-app/pages/contact.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Contact.module.css";
+
+function Contact() {
+  return <h2 className={styles.test}>Contact page</h2>;
+}
+
+export default Contact;
---
+++ my-app/pages/service.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Service.module.css";
+
+function Service() {
+  return <h1 className={`${styles.test} ${styles.mainFont}`}>Service page</h1>;
+}
+
+export default Service;
